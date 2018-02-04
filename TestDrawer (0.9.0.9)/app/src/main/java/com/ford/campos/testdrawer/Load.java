efb30397855c91c64c94fd5643b6db0fe4d33123
ethan <ethcf@yahoo.com>
2018-02-04 08:42:40 -0800

    Refactoring
    Split gigantor methods into smaller, more managable methods!
    
    Also added some documentation/comments.

--- TestDrawer (0.9.0.9)/app/src/main/java/com/ford/campos/testdrawer/Load.java
+++ TestDrawer (0.9.0.9)/app/src/main/java/com/ford/campos/testdrawer/Load.java
@@ -1,6 +1,7 @@
 package com.ford.campos.testdrawer;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.util.Log;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -8,63 +9,117 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
+/**
+ * Mostly refactored on: 2/4/18
+ *
+ * Todo: fix setMusicHolderArraySize(); it is inexcusable.
+ *
+ */
+
 public class Load {
 
     private final String TAG = "Load";
     private Context context;
 
+    private String filename;
+    private ArrayList<String> linksArray;
+
     public Load(Context c) {
         context = c;
+        linksArray = new ArrayList<>();
     }
 
-    // Gets the links from the txt file and sends back an arrayList with
-    // the links in it, if the file doesn't exist we return null
+    /**
+     * Gets the URL's from the specified txt file and sends back an ArrayList with
+     * those links. If the file doesn't exist we return null.
+     *
+     * @param filename the file we're trying to load (not including ".txt")
+     * @return if the file exists: an ArrayList with the URL's in the file; otherwise null.
+     */
     public ArrayList<String> load(String filename) {
+        setup(filename);
+        setLinksArray();
+        setMusicHolderArraySize();
+        return linksArray;
+    }
 
-        //This shouldn't happen; if it does... well, we know something is wrong
-        if (!hasFile(context, filename)) {
+    private void setup(String filename) {
+        this.filename = filename;
+        this.filename += ".txt";
+        checkIfFileExists();
+    }
+
+    /**
+     * This should never throw a runtime exception, if it does... well, we know something has gone
+     * horribly wrong.
+     */
+    private void checkIfFileExists() {
+        if (!hasFile(context)) {
             Log.wtf( TAG, "Trying to load a non-existent file", new RuntimeException() );
         }
+    }
 
-        filename += ".txt";
+    private boolean hasFile(Context context) {
+        return context.getFileStreamPath(filename).exists();
+    }
 
-        ArrayList<String> linksArray = new ArrayList<>();
-
-        try { linksArray = getLinksFromFile(filename); }
+    /**
+     * Wrapper method that adds all the url's in the specified file to the AraryList
+     *
+     * @throws RuntimeException if the URL's weren't able to be retrieved
+     */
+    private void setLinksArray() {
+        try { getLinksFromFile(); }
         catch (IOException e) {
-            Log.wtf(TAG, "What the fuck, why am I here?", new RuntimeException());
+            Log.wtf(TAG, "couldn't get links from file", new RuntimeException());
         }
+    }
 
-        int index = MainActivityHelper.getPositionForArray(filename);
-        Log.d(TAG, filename + " loaded " + linksArray.size());
-        ( (MainActivity)context ).setOriginalSizes(index, linksArray.size());
+    /**
+     * Sets everything up for the file to be read, then calls addUrlsToLinksArray() to
+     * do the actual work.
+     *
+     * @throws IOException if the file wasn't able to be opened
+     */
+    private void getLinksFromFile() throws IOException {
 
-        return linksArray;
+        InputStream in = context.openFileInput(filename);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+        addUrlsToLinksArray(reader);
+        closeReader(reader);
 
     }
 
-    private ArrayList<String> getLinksFromFile(String filename) throws IOException {
-
-        InputStream in = context.openFileInput(filename);
-        ArrayList<String> linksArray = new ArrayList<>();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-
+    /**
+     * Adds a bunch of URL's to the linksArray.
+     *
+     * @throws IOException if the file wasn't able to be opened
+     */
+    private void addUrlsToLinksArray(BufferedReader reader) throws IOException {
         String line;
-
         while ((line = reader.readLine()) != null)
             linksArray.add(line);
+    }
 
+    private void closeReader(BufferedReader reader) {
         if (reader != null) {
             Log.d(TAG, "reader != null, closing");
             try { reader.close(); }
             catch (IOException e) { Log.d(TAG, "reader!=null, IOException"); }
         }
-
-        return linksArray;
     }
 
-    private boolean hasFile(Context context, String filename) {
-        return context.getFileStreamPath(filename + ".txt").exists();
+    /**
+     * Todo: fix this!
+     *
+     * This is really bad, it'll need to be refactored; BUT the rest of the app needs to be
+     * refactored first, so ill come back to it later
+     */
+    private void setMusicHolderArraySize() {
+        int index = MainActivityHelper.getPositionForArray(filename);
+        Log.d(TAG, filename + " loaded " + linksArray.size());
+        ( (MainActivity)context ).setOriginalSizes(index, linksArray.size());
     }
 
 }
